bbd7658607b005e7c7878b36a567bdac2d9b5738
saurabhmaurya259 <saurabhmaurya0654@gmail.com>
2026-08-03 13:19:07 +0530

    commneted the start now section

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -25,7 +25,9 @@ const HIGHLIGHTS = [
 const CTA = () => {
   return (
     <div>
-      <div className="flex rounded-b-[70px] z-10 flex-col bg-white items-center text-center relative gap-4 py-20 lg:py-32 overflow-hidden ">
+      {/*
+      <div className="flex rounded-b-[70px] z-10 flex-col bg-white items-center text-center relative gap-4 py-20 lg:py-32 overflow-hidden">
+
         <AnimationContainer
           animation="scaleUp"
           delay={0.2}
@@ -51,37 +53,47 @@ const CTA = () => {
         </AnimationContainer>
 
         <div className="flex flex-col items-center justify-center w-full z-30">
+
           <AnimationContainer animation="fadeUp" delay={0.3}>
             <SectionBadge title="Start now" />
           </AnimationContainer>
 
           <AnimationContainer animation="fadeUp" delay={0.55}>
             <div className="mt-10 mb-8 max-w-xl">
+
               <blockquote className="relative">
                 <div className="absolute -top-6 left-0 text-5xl text-primary opacity-30">
                   "
                 </div>
+
                 <p className="italic text-lg md:text-xl lg:text-3xl font-light text-transparent bg-clip-text bg-gradient-to-r from-primary to-neutral-700">
-                  A house is made of bricks and beams. A home is made of hopes
-                  and dreams.
+                  A house is made of bricks and beams.
+                  A home is made of hopes and dreams.
+
                   <span className="block text-xl mt-1 font-medium">
                     We Build Your Home
                   </span>
                 </p>
+
                 <div className="absolute -bottom-6 right-0 text-5xl text-primary opacity-30">
                   "
                 </div>
               </blockquote>
+
               <div className="flex justify-center mt-4">
                 <div className="h-px w-16 bg-gradient-to-r from-primary/20 via-primary to-primary/20"></div>
               </div>
+
               <p className="text-lg font-medium mt-3 text-neutral-600">
                 CEO, CCS Infratech
               </p>
+
             </div>
           </AnimationContainer>
+
         </div>
       </div>
+      */}
     </div>
   );
 };
